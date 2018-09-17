4cf49ad6fd83ea438be21ad42a1aedeb14ef5252
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air.local>
2018-09-17 14:08:56 +0100

    able to shuffle

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', function(){
   // card is getting called from HTML
   let card = document.getElementsByClassName("card");
-  // the array cards is going to hold cards
-  // spread operator(...) will display all the items in an array
-  // let cards = [...card]
 
   // card-pack is being called from the HTML where all the cards are being held
   let pack = document.getElementById("card-pack");
@@ -15,6 +12,34 @@ document.addEventListener('DOMContentLoaded', function(){
 
   //matchcard is being declared and match is being called from the HTML
   let matchCard = document.getElementsByClassName("match");
+
+  //closes the icon in the modal when message pops up
+  let closeicon = document.querySelector(".close");
+
+  //this declares the modal and it called from HTML
+  let modal = document.getElementById("popup1")
+
+  //array for opened cards that have been clicked on
+  var openCards = [];
+  // declaring the variables
+  function shuffle(array) {
+    var currentIndex = array.length;
+    var temporaryValue;
+    var randomIndex;
+    //this will shuffle the cards, so that they are in different places
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    //this will return the shuffled array
+    return array;
+};
+
+console.log(shuffle(openCards));
+
 
 
 
