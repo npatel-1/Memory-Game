3c6040dc205bef5d77cd56cdb7a71dfa4bce17ac
Tech-A03 <tech-a03@Tech-A03s-MacBook-Air.local>
2018-09-17 19:59:38 +0100

    matching cards

--- js/app.js
+++ js/app.js
@@ -22,27 +22,88 @@ document.addEventListener('DOMContentLoaded', function(){
   //array for opened cards that have been clicked on
   var openCards = [];
   // declaring the variables
-  function shuffle(array) {
-    var currentIndex = array.length;
+  function shuffle(cardArray) {
+    var currentIndex = cardArray.length;
     var temporaryValue;
     var randomIndex;
-    //this will shuffle the cards, so that they are in different places
+    //this will shuffle the cardArrays, so that they are in different places
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
         currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
+        temporaryValue = cardArray[currentIndex];
+        cardArray[currentIndex] = cardArray[randomIndex];
+        cardArray[randomIndex] = temporaryValue;
     }
     //this will return the shuffled array
-    return array;
+    return cardArray;
+};
+//cards are shuffled when the page reloads
+document.body.onload = startGame();
+//the function to restart the game
+function startGame(){
+  //this is where the cards will be stored
+  let tempArray = [];
+  console.log(card);
+  //loop is created for the lists
+  for (var i = 0; i < 16; i++) {
+    //cardEl is declared and an list element is created
+    const cardEl = document.createElement('li');
+    //list is then given a class and id
+    cardEl.setAttribute('class','card');
+    cardEl.setAttribute('id',`card-${i}`);
+    //cardel is then pushed into tempArray
+    tempArray.push(cardEl);
+    // card[i].innerHTML = i;
+  }
+  //tempArray is then shuffled
+  shuffledArray = shuffle(tempArray);
+  //shuffledArray is then displayed shuffled
+  for (var i = 0; i < shuffledArray.length; i++) {
+    //the lists in the html will then return the id
+    shuffledArray[i].innerHTML = shuffledArray[i].id;
+    //the pack will then add the list
+    pack.appendChild(shuffledArray[i])
+  }
+
+  console.log(tempArray);
+}
+//reset the moves
+moves = 0;
+counter.innerHTML = moves;
+//reset the time
+second = 0;
+minutes = 0;
+var timer = document.querySelector(".time");
+timer.innerHTML = "0 mins 0 secs";
+
+//toggles open and show class to display opencards
+var displayCard = function(){
+  this.classList.toggle("open");
+  this.classList.toggle("show");
+  this.classList.toggle("disabled");
+}
+
+// opened cards are added to openCards list and is then checked to see if cards match
+function cardOpen() {
+  openCards.push(this);
+  var length = openCards.length;
+  if (length === 2) {
+    moveCounter();
+    if (openCards[0].type === openCards[1].type) {
+        matched();
+    } else {
+        unmatched();
+    }
+  }
 };
 
-console.log(shuffle(openCards));
-
-
-
-
-
+// $('.btnRestart').click(function() {
+//   //window.location.reload(true);
+//   history.go(0);
+// });
+//
+// window.onload = function() {
+//   $('.card').text($.now());
+// };
 
 }) // DOMContentLoaded Close
